912890173a0352b4afbcef15535766b2ac289281
Miguel <fagundezdev@gmail.com>
2020-07-30 21:15:18 -0400

    new custom dialog new image

--- app/src/main/java/com/devproject/miguelfagundez/customalertdialog/CustomDialog.java
+++ app/src/main/java/com/devproject/miguelfagundez/customalertdialog/CustomDialog.java
@@ -20,7 +20,8 @@ public class CustomDialog {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 
         LayoutInflater inflater = activity.getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.custom_dialog, null));
+        //builder.setView(inflater.inflate(R.layout.custom_dialog, null));
+        builder.setView(inflater.inflate(R.layout.custom_dialog_two, null));
         builder.setCancelable(true);
 
         dialog = builder.create();
